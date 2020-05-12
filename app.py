903e86b966b4c1510880bb6ccc03d8e12533b8b4
Gabriel Campanella <gabe.camp@gmail.com>
2020-05-11 23:36:12 -0400

    handling request from reactjs

--- app.py
+++ app.py
@@ -1,29 +1,34 @@
 from flask import Flask, request
 from movienight import MovieNight
-
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/', methods=['POST'])
 @app.route('/movies', methods=['POST'])
 def movies():
     req_data = request.get_json()
-    months = 1
-    if 'months' in req_data:
-        months = req_data['months']
-    actors = []
-    if 'actors' in req_data:
-        actors = req_data['actors']
-    genres = []
-    if 'genres' in req_data:
-        genres = req_data['genres']
-    directors = []
-    if 'directors' in req_data:
-        directors = req_data['directors']
+    try:
+        months = 1
+        if 'months' in req_data:
+            months = req_data['months']
+        actors = []
+        if 'actors' in req_data:
+            actors = req_data['actors']
+        genres = []
+        if 'genres' in req_data:
+            genres = req_data['genres']
+        directors = []
+        if 'directors' in req_data:
+            directors = req_data['directors']
+    except TypeError:
+        #print('{}\n{}\n{}'.format(request.headers,request.get_data(), request.get_json()))
+        return None
     mn = MovieNight(months=months, actors=actors, genres=genres, directors=directors)
     r = mn.getMovies()
     if r > 0:
+        print('{}'.format(mn.toJSON()))
         return mn.toJSON()
     else:
         return None
